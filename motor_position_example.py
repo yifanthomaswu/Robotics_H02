254fc5fff3f93446271c86e54d44436aee27a7fd
Yifan Wu <yw8012@ic.ac.uk>
2016-10-15 16:33:41 +0100

    Tuning motors

--- motor_position_example.py
+++ motor_position_example.py
@@ -9,32 +9,46 @@ motors = [0,1]
 interface.motorEnable(motors[0])
 interface.motorEnable(motors[1])
 
-motorParams = interface.MotorAngleControllerParameters()
-motorParams.maxRotationAcceleration = 6.0
-motorParams.maxRotationSpeed = 12.0
-motorParams.feedForwardGain = 255/20.0
-motorParams.minPWM = 18.0
-motorParams.pidParameters.minOutput = -255
-motorParams.pidParameters.maxOutput = 255
-motorParams.pidParameters.k_p = 100.0
-motorParams.pidParameters.k_i = 0.0
-motorParams.pidParameters.k_d = 0.0
+motorParams0 = interface.MotorAngleControllerParameters()
+motorParams0.maxRotationAcceleration = 6.0
+motorParams0.maxRotationSpeed = 12.0
+motorParams0.feedForwardGain = 255/17.0
+motorParams0.minPWM = 25.0
+motorParams0.pidParameters.minOutput = -255
+motorParams0.pidParameters.maxOutput = 255
+motorParams0.pidParameters.k_p = 600.0
+motorParams0.pidParameters.k_i = 500.0
+motorParams0.pidParameters.k_d = 200.0
 
-interface.setMotorAngleControllerParameters(motors[0],motorParams)
-interface.setMotorAngleControllerParameters(motors[1],motorParams)
+motorParams1 = interface.MotorAngleControllerParameters()
+motorParams1.maxRotationAcceleration = 6.0
+motorParams1.maxRotationSpeed = 12.0
+motorParams1.feedForwardGain = 255/17.0
+motorParams1.minPWM = 25.0
+motorParams1.pidParameters.minOutput = -255
+motorParams1.pidParameters.maxOutput = 255
+motorParams1.pidParameters.k_p = 600.0
+motorParams1.pidParameters.k_i = 500.0
+motorParams1.pidParameters.k_d = 200.0
+
+interface.setMotorAngleControllerParameters(motors[0],motorParams0)
+interface.setMotorAngleControllerParameters(motors[1],motorParams1)
+
 
 while True:
-	angle = float(input("Enter a angle to rotate (in radians): "))
+    angle = float(input("Enter a angle to rotate (in radians): "))
 
-	interface.increaseMotorAngleReferences(motors,[angle,angle])
-
-	while not interface.motorAngleReferencesReached(motors) :
-		motorAngles = interface.getMotorAngles(motors)
-		if motorAngles :
-			print "Motor angles: ", motorAngles[0][0], ", ", motorAngles[1][0]
-		time.sleep(0.1)
-
-	print "Destination reached!"
-	
-
+    interface.increaseMotorAngleReferences(motors,[angle,angle])
+    interface.startLogging("log.txt")
+    
+    while not interface.motorAngleReferencesReached(motors) :
+        motorAngles = interface.getMotorAngles(motors)
+        if motorAngles :
+            print "Motor angles: ", motorAngles[0][0], ", ", motorAngles[1][0]
+        time.sleep(0.1)
+    
+    interface.stopLogging()
+    print "Destination reached!"
+    
+    
 interface.terminate()
--- plotlog.py
+++ plotlog.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-with open("log.txt") as f:
+with open("652.txt") as f:
     data = f.read()
 
 data = data.split('\n')
@@ -17,19 +17,35 @@ del y2[-1]
 y3 = [row.split('\t')[4] for row in data]
 del y3[-1]
 
-fig, ax1 = plt.subplots()
-
-ax1.set_title("Plot title...")
-ax1.set_xlabel('Time')
-ax1.set_ylabel('Angle')
-
-ax2 = ax1.twinx()
-
+f, (ax1, ax2) = plt.subplots(1, 2)
 ax1.plot(x,y0, 'r')
 ax1.plot(x,y1, 'r')
-ax2.plot(x,y2, 'g')
-ax2.plot(x,y3, 'g')
+ax3 = ax1.twinx()
+ax3.plot(x,y2, 'g')
+ax3.plot(x,y3, 'g')
 
-leg = ax1.legend()
+for i in range(0,len(y0)):
+  y0[i] = float(y0[i]) - float(y1[i])
+for i in range(0,len(y2)):
+  y2[i] = float(y2[i]) - float(y3[i])
+
+ax2.plot(x,y0, 'r')
+ax2.plot(x,y2, 'g')
+
+
+# fig, ax1 = plt.subplots()
+
+# ax1.set_title("Plot title...")
+# ax1.set_xlabel('Time')
+# ax1.set_ylabel('Angle')
+
+# ax2 = ax1.twinx()
+
+# ax1.plot(x,y0, 'r')
+# ax1.plot(x,y1, 'r')
+# ax2.plot(x,y2, 'g')
+# ax2.plot(x,y3, 'g')
+
+# leg = ax1.legend()
 
 plt.show()
